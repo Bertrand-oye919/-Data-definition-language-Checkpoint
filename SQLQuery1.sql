a12a0bf6a370e630b11a18fc048309b647268410
Bertrand-oye919 <126890195+Bertrand-oye919@users.noreply.github.com>
2025-08-02 22:58:40 +0100

    adding data manipulation language screenshots

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,6 +7,11 @@ CREATE TABLE department (
     label VARCHAR(225) NOT NULL,
     manager_name VARCHAR(225) NOT NULL
 );
+INSERT INTO department (num_s, label, manager_name) VALUES
+(1, 'IT', 'Alice Johnson'),
+(2, 'HR', 'Bob Smith'),
+(3, 'Marketing', 'Clara Bennett'
+);
 
 -- Create Employee table
 CREATE TABLE employee (
@@ -19,6 +24,14 @@ CREATE TABLE employee (
         ON DELETE SET NULL 
         ON UPDATE CASCADE
 );
+
+INSERT INTO employee (num_e, name, position, salary, department_num_s) VALUES
+(101, 'John Doe', 'Developer', 60000.00, 1),
+(102, 'Jane Smith', 'Analyst', 55000.00, 2),
+(103, 'Mike Brown', 'Designer', 50000.00, 3),
+(104, 'Sarah Johnson', 'Data Scientist', 70000.00, 1),
+(105, 'Emma Wilson', 'HR Specialist', 52000.00, 2);
+
 
 -- Create Project table
 CREATE TABLE project (
@@ -33,6 +46,13 @@ CREATE TABLE project (
     CHECK (end_date >= start_date)
 );
 
+INSERT INTO project (num_p, title, start_date, end_date, department_num_s) VALUES
+(201, 'Website Redesign', '2024-01-15', '2024-06-30', 1),
+(202, 'Employee Onboarding', '2024-03-01', '2024-09-01', 2),
+(203, 'Market Research', '2024-02-01', '2024-07-31', 3),
+(204, 'IT Infrastructure Setup', '2024-04-01', '2024-12-31', 1);
+
+
 -- Create Employee_Project table
 CREATE TABLE employee_project (
     employee_num_e INT NOT NULL,
@@ -46,5 +66,22 @@ CREATE TABLE employee_project (
         ON DELETE NO ACTION 
         ON UPDATE NO ACTION
 );
+INSERT INTO employee_project (employee_num_e, project_num_p, role) VALUES
+(101, 201, 'Frontend Developer'),
+(104, 201, 'Backend Developer'),
+(102, 202, 'Trainer'),
+(105, 202, 'Coordinator'),
+(103, 203, 'Research Lead'),
+(101, 204, 'Network Specialist');
 
 
+'''modifying table employee_project with employee_num_e and project_num_p where it is = 101 and 201 
+respectfully and changing the role from frontend developer to full stack developer'''
+
+UPDATE employee_project
+SET role = 'Full Stack Developer'
+WHERE employee_num_e = 101 AND project_num_p = 201;
+
+
+DELETE FROM employee
+WHERE num_e = 103;
